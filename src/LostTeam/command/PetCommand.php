78d0aeb656a3936089ca862a140da2933854c4bc
XakerS <enderman.com290@gmail.com>
2016-05-25 14:39:55 +0400

    Mmm...
    Russian language (need translate)
    And support by Adam
    Thanks Adam!

--- src/LostTeam/command/PetCommand.php
+++ src/LostTeam/command/PetCommand.php
@@ -1,98 +1,206 @@
 <?php
-namespace LostTeam\command;
 
-use LostTeam\Main;
+namespace LostTeam\command;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
-use pocketmine\Player;
-use pocketmine\plugin\Plugin;
+use pets\main;
 use pocketmine\utils\TextFormat as TF;
 
 class PetCommand extends PluginCommand {
-	public $main, $current;
 
-	/**
-	 * PetCommand constructor.
-	 * @param Main $main
-	 * @param $name
-     */
-	public function __construct(Main $main, $name) {
-		parent::__construct($name, $main);
+	public function __construct(main $main, $name) {
+		parent::__construct(
+				$name, $main
+		);
 		$this->main = $main;
-		$this->setPermission("pet.command");
-		$this->setAliases(["pet"]);
+		$this->setPermission("pets.command");
+		$this->setAliases(array("pet"));
 	}
 
-	/**
-	 * @param CommandSender $sender
-	 * @param string $currentAlias
-	 * @param array $args
-	 * @return bool
-     */
 	public function execute(CommandSender $sender, $currentAlias, array $args) {
-		if(!$sender instanceof Player) {
-			$sender->sendMessage("Only Players can use this plugin");
-			return true;
-		}
+	
 		if (!isset($args[0])) {
-			if($sender->hasPermission('pet.command.help')) {
-				$sender->sendMessage(TF::YELLOW."=======".TF::BLUE."Pets".TF::YELLOW."=======");
-				$sender->sendMessage(TF::YELLOW."/pets ");
-				$sender->sendMessage(TF::YELLOW."/pets cycle");
+			if($sender->hasPermission('pets.command')){
+				$this->main->togglePet($sender);
 				return true;
-			}else{
-				$sender->sendMessage(TF::RED . "You do not have permission to use this command");
 			}
-			return true;
+			else{
+				$sender->sendMessage(TextFormat::RED."Для этой команды нужна привелегия!§6Подробнее тут:§a 1.sky-wars.ru");
+				return true;
+			}
 		}
 		switch (strtolower($args[0])){
 			case "name":
-			case "setname":
-				if(!$sender->hasPermission('pet.command.name')) {
-					$sender->sendMessage(TF::RED . "You do not have permission to use this command");
-				}
 				if (isset($args[1])){
-//					unset($args[0]);
+					unset($args[0]);
 					$name = implode(" ", $args);
 					$this->main->getPet($sender->getName())->setNameTag($name);
-					$sender->sendMessage("Set Name to ".$name);
+					$sender->sendMessage("§Теперь твоего питомца зовут:§a".$name);
 				}
+				return true;
 			break;
+			
+			case "list":
+				if($sender->hasPermission('pets.command.list')){
+				$sender->sendMessage("§e======§aСписок животных(типов)§e======");
+				$sender->sendMessage("§e§ldog/wolf §aСобачка");
+				$sender->sendMessage("§e§lblaze §aБлэйз");
+				$sender->sendMessage("§e§lpig §aХрюшка");
+				$sender->sendMessage("§e§lchicken §aКурочка");
+				$sender->sendMessage("§e§lrabbit §aКролик");
+				$sender->sendMessage("§e§lmagma §aМагмовый куб");
+				$sender->sendMessage("§e§lbat §aЛетучая мышь");
+				$sender->sendMessage("§e§lsilverfish §aРыбка");
+				$sender->sendMessage("§e§lcat/ocelot §aКотик");
+				$sender->sendMessage("§e§lslime §aСлайм");
+				return true;
+				}
+				else{
+				$sender->sendMessage("§4Для этой команды нужна привелегия!§6Подробнее тут:§a 1.sky-wars.ru");
+				 }
+				return true;
+			break;
+			
 			case "help":
-				if(!$sender->hasPermission('pet.command.help')) {
-					$sender->sendMessage(TF::RED . "You do not have permission to use this command");
-				}
-					$sender->sendMessage(TF::YELLOW."=======".TF::BLUE."Pets".TF::YELLOW."=======");
-					$sender->sendMessage(TF::YELLOW."/pets help");
-					$sender->sendMessage(TF::YELLOW."/pets cycle");
-					$sender->sendMessage(TF::YELLOW."/pets name <Pet Name>");
-			break;
-			case "cycle":
-				if(!$sender->hasPermission('pet.command.cycle')) {
-					$sender->sendMessage(TF::RED . "You do not have permission to use this command");
-				}
-				$types = array("ChickenPet","PigPet","WolfPet","BlazePet","RabbitPet","BatPet","SilverfishPet","MagmaPet");
-				$new = null;
-				if($this->current != count($types)-1) {
-					$new = $this->current+1;
+				if($sender->hasPermission('pet.command.help')){
+				$sender->sendMessage("§e======§aПомощь§e======");
+				$sender->sendMessage("§b/pets §aВключить/§4выключить ");
+				$sender->sendMessage("§b/pets type [тип] §6изменить тип питомца");
+				$sender->sendMessage("§b/pets name [новое имя] §6Сменить имя");
+				$sender->sendMessage("§b/pets list §6Список (тип) животных");
+				return true;
 				}else{
-					$new = 0;
+					$sender->sendMessage(TextFormat::RED."Для этой команды нужна привелегия!§6Подробнее тут:§a 1.sky-wars.ru");
 				}
-				$this->main->changePet($sender, $types[$new]);
-			break;
-			default:
-				if($sender->hasPermission('pet.command.help')) {
-					$sender->sendMessage(TF::YELLOW."=======".TF::BLUE."Pets".TF::YELLOW."=======");
-					$sender->sendMessage(TF::YELLOW."/pets ");
-					$sender->sendMessage(TF::YELLOW."/pets cycle");
 					return true;
-				}else{
-					$sender->sendMessage(TF::RED . "You do not have permission to use this command");
+			break;
+			
+			case "type":
+				if (isset($args[1])){
+					switch ($args[1]){
+						case "dog":
+							if ($sender->hasPermission("pets.type.dog")){
+								$this->main->changePet($sender, "WolfPet");
+								$sender->sendMessage("§6Теперь твой питомец §a");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "chicken":
+							if ($sender->hasPermission("pets.type.chicken")){
+								$this->main->changePet($sender, "ChickenPet");
+								$sender->sendMessage("§6Теперь твой питомец §aкурица");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "blaze":
+							if ($sender->hasPermission("pets.type.blaze")){
+								$this->main->changePet($sender, "BlazePet");
+								$sender->sendMessage("§6Теперь твой питомец §aблейз");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав на этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						case "magma":
+							if ($sender->hasPermission("pets.type.magma")){
+								$this->main->changePet($sender, "MagmaPet");
+								$sender->sendMessage("§6Теперь твой питомец §aмагмовый куб");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "rabbit":
+							if ($sender->hasPermission("pets.type.rabbit")){
+								$this->main->changePet($sender, "RabbitPet");
+								$sender->sendMessage("Теперь твой питомец §aкролик");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "bat":
+							if ($sender->hasPermission("pets.type.bat")){
+								$this->main->changePet($sender, "BatPet");
+								$sender->sendMessage("§6Теперь твой питомец §aлетучая мышь");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "pig":
+							if ($sender->hasPermission("pets.type.pig")){
+								$this->main->changePet($sender, "PigPet");
+								$sender->sendMessage("§6Теперь твой питомец §aхрюшка");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "silverfish":
+							if ($sender->hasPermission("pets.type.silerfish")){
+								$this->main->changePet($sender, "SilverfishPet");
+								$sender->sendMessage("§6Теперь твоя питомец §aрыбка");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "ocelot":
+							if ($sender->hasPermission("pets.type.ocelot")){
+								$this->main->changePet($sender, "OcelotPet");
+								$sender->sendMessage("Теперь твой питомец §aкотик :3");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+						
+						case "Slime":
+							if ($sender->hasPermission("pets.type.slime")){
+								$this->main->changePet($sender, "SlimePet");
+								$sender->sendMessage("§6Теперь твой питомец §aслайм");
+								return true;
+							}else{
+								$sender->sendMessage("§4У тебя нету прав для этого питомца!§6Купить их можно тут:§a1.sky-wars.ru");
+								return true;
+							}
+						break;
+					}
+					
 				}
+				else{
+					$sender->sendMessage("§b/pet type [тип]");
+					$sender->sendMessage("§6Типы:§a ocelot, slime,blaze, pig, chicken, dog, rabbit, magma, bat, silverfish");
+					return true;
+					}
+				return true;
 			break;
 		}
+		
 		return true;
 	}
 
 }
+
